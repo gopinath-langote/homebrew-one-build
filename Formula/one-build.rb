0a85561373547b3992e648ffa22ab6d5ef4d84f5
Gopinath Langote <gopinath.langote@phonepe.com>
2026-03-02 15:26:47 +0530

    Fix checksums for v1.10.4 formula

--- Formula/one-build.rb
+++ Formula/one-build.rb
@@ -9,20 +9,20 @@ class OneBuild < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/gopinath-langote/1build/releases/download/v1.10.4/1build_Darwin_x86_64.tar.gz"
-      sha256 "9e375c9eafbde547f23980a3c5eb5b86ed19f153d18b79459989148604b579c0"
+      sha256 "2bcdd4dc81d3a9d55b69fc852c260cafa3fbca097b02dcb838287c75d90b2527"
     elsif Hardware::CPU.arm?
       url "https://github.com/gopinath-langote/1build/releases/download/v1.10.4/1build_Darwin_arm64.tar.gz"
-      sha256 "2345a94307489c70469ff6fb64b8c5436ec6fa78aef8b960ccf47cf20897ce98"
+      sha256 "a6c290fe4e3d0fac7329af4f88bbf949e1677c2315fe3fb8517fd8a687f200d3"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/gopinath-langote/1build/releases/download/v1.10.4/1build_Linux_x86_64.tar.gz"
-      sha256 "8d83f9c073d2b59e70060fbc0d4e870e56f1d071f51a3801504447413055bb45"
+      sha256 "28ced94ee3f502d173e8930381650ed7da67e7d0d998f984432c36a3f6f3fa16"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/gopinath-langote/1build/releases/download/v1.10.4/1build_Linux_arm64.tar.gz"
-      sha256 "228b9a5c557509927ef4acf920b5f6540c50f37a759b7e1613ce974f1d8b5015"
+      sha256 "1a91ffcca29c80f52c9680d2d8ebdcfeb70142d7ababa6bc4b492b4a4a29681d"
     end
   end
 
